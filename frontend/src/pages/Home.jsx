e70f94f23ae82d75a28e87244d31092ad51c5707
Rafay <rafayn651@gmail.com>
2025-11-16 02:36:02 +0500

    fix: correct Features import path for case-sensitive Vercel build

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 
 
 import HeroSection from '../components/Hero'
-import Features from '@/components/features'
+import Features from '@/components/Features'
 import PortalsSection from '@/components/PortalsSection'
 import AboutSnippet from '@/components/AboutSnippet'
 import FooterSection from '@/components/Footer'
